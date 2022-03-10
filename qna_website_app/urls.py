3d57a39da17df8495fa688cd25ca0989fe83dce5
TarunSinghIIT <singh.tarun.iit@gmail.com>
2022-03-10 22:55:32 +0530

    made important changes to views

--- qna_website_app/urls.py
+++ qna_website_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import AllAnswers, LogIn, NewUser, QuestionList, QuestionAdd, QuestionInfo, QuestionChange, QuestionDelete, AllQuestions
+from .views import AllAnswers, AnswerInfo, LogIn, NewUser, QuestionList, QuestionAdd, QuestionInfo, QuestionChange, QuestionDelete, AllQuestions
 from django.contrib.auth.views import LogoutView
 
 from . import views
@@ -11,9 +11,10 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('new_user/', NewUser.as_view(), name='newuser'),
     path('question_add/', QuestionAdd.as_view(), name='question_add'),
-    path('question/<int:pk>/', QuestionInfo.as_view(), name='question'),
+    path('question/<int:question_id>/', views.questioninfo, name='question'),
     path('question_change/<int:pk>/', QuestionChange.as_view(), name='question_change'),
     path('task-delete/<int:pk>/', QuestionDelete.as_view(), name='question_delete'),
-    path('all_questions/', AllQuestions.as_view(), name='all_questions'),
-    path('all_answers/', AllAnswers.as_view(), name='all_answers')
+    path('all_questions/', views.questionlist, name='all_questions'),
+    path('all_answers/', AllAnswers.as_view(), name='all_answers'),
+    path('answer/<int:answer_id>/', AnswerInfo.as_view(), name='answer'),
 ]
